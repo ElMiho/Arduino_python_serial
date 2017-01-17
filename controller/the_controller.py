abf99f2dcd8042c4d0cca4f7a106b42a51f3a35d
ElMiho <mikael.hjermitslev@gmail.com>
2017-01-17 14:35:01 +0000

    Removed debug

--- controller/the_controller.py
+++ controller/the_controller.py
@@ -43,7 +43,7 @@ def action():
         return render_template('turned_off.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
 
 
 def echo(input):
