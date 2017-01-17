2d4166be55ebefde1ff430c64c533bc383215f67
ElMiho <mikael.hjermitslev@gmail.com>
2017-01-17 12:14:01 +0000

    Made it pretty on the web app

--- controller/the_controller.py
+++ controller/the_controller.py
@@ -1,39 +1,45 @@
 import serial
-port = raw_input("Please enter port: ")
+#port = raw_input("Please enter port: ")
 ser = serial.Serial()
-ser.port = port
+ser.port = "/dev/cu.usbmodem1421"
 ser.open()
 
-from flask import Flask, request
-app = Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__, static_folder='/static')
+
+state = " "
 
 @app.route("/")
 def hello():
-        return "hello"
+    return "hello"
 
 @app.route("/turn_on")
 def on():
-    ser.write("onX")
+    state = "onX"
+    ser.write(state)
     return "LED is on"
 
 @app.route("/turn_off")
 def off():
-    ser.write("offX")
+    state = "offX"
+    ser.write(state)
     return "LED is off"
 
 @app.route("/test")
 def test():
-    return '<form action="/action" method="POST"><input name="text"><input type="submit" value="Echo"></form>'
+    return render_template('simple_template.html')
 
 @app.route("/action", methods = ['POST'])
 def action():
     text = request.form.get('text')
     if text == "turn_on":
-        ser.write("onX")
-        return "LED is on"
+        state = "onX"
+        ser.write(state)
+        return render_template('turned_on.html')
     elif text == "turn_off":
-        ser.write("offX")
-        return "LED is off"
+        state = "offX"
+        ser.write(state)
+        return render_template('turned_off.html')
     else:
         return str(text).upper()
 
